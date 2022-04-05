b04408aaeec4bf540b70c71f646ce99e15b08248
EliMeitner <eli.meitner@toluna.com>
2022-04-05 09:21:45 +0300

    Update lambda.go, s3.go, and s3.go

---
+++ modules/aws/lambda/lambda.go
@@ -0,0 +1,24 @@
+package tolunalambda
+
+import (
+	"testing"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/lambda"
+	aws_terratest "github.com/gruntwork-io/terratest/modules/aws"
+	"github.com/stretchr/testify/assert"
+)
+
+func GetLambdaLayer(t *testing.T, region string, layer_name string, layer_version int64) *lambda.GetLayerVersionOutput {
+	sess, err := aws_terratest.NewAuthenticatedSession(region)
+	svc := lambda.New(sess)
+	input := &lambda.GetLayerVersionInput{
+		LayerName:     aws.String(layer_name),
+		VersionNumber: aws.Int64(layer_version),
+	}
+	result, err := svc.GetLayerVersion(input)
+	if err != nil {
+		assert.Nil(t, err, "Failed to get layer")
+	}
+	return result
+}
