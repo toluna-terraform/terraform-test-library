4b485ec8c81e4adb890fd43b6f742062bdc6014e
SrinivasaRao Asomayajula <srinivasarao.asomayajula@toluna.com>
2022-04-25 17:23:38 +0530

    clean up redundent code

--- modules/aws/vpc/vpc.go
+++ modules/aws/vpc/vpc.go
@@ -2,19 +2,16 @@ package tolunavpcaws
 
 import (
     "testing"
-    //"encoding/json"
 
-    //"github.com/aws/aws-sdk-go/service/s3"
     aws_terratest "github.com/gruntwork-io/terratest/modules/aws"
     "github.com/stretchr/testify/assert"
 )
 
-var region = "us-east-1"
-var vpcId = "vpc-0a9d3172533527c76"
-
-func TestVpcExists(t *testing.T) {
+// tests if given VPC exists 
+func TestIfVpcExists(t *testing.T, vpcId string, region string) {
     vpc := aws_terratest.GetVpcById(t, vpcId, region)
     assert.NotEmpty(t, vpc)
 }
+
 
 
